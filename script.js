fb018da78eb94ac1b178c0d5f869866d82dc6671
unknown <greglock1997@outlook.com>
2023-02-16 18:21:55 +0900

    Added navbar and updated skills page

---
+++ script.js
@@ -0,0 +1,45 @@
+/* NAVBAR */
+
+// RESET POSITIONS AFTER RELOADING PAGE
+window.scrollTo(0, 0);
+
+// DEFINE NAVBAR ELEMENTS
+const navbarLinksHome = document.querySelector('#navbar-home');
+const navbarLinksSkills = document.querySelector('#navbar-skills');
+const navbarLinksProjects = document.querySelector('#navbar-projects');
+const navbarLinksContact = document.querySelector('#navbar-contact');
+
+// DEFINE PAGE POSITIONS
+const homePosition = document.querySelector('.aboutMe').getBoundingClientRect();
+const skillsPosition = document.querySelector('.skills').getBoundingClientRect();
+const projectsPosition = document.querySelector('.projects').getBoundingClientRect();
+const contactPosition = document.querySelector('.contact').getBoundingClientRect();
+
+// ADD SCROLL EVENT LISTENERS TO LINKS
+navbarLinksHome.addEventListener('click', function() {
+    window.scrollTo({ top: homePosition.y, behavior: 'smooth' });    
+});
+
+navbarLinksSkills.addEventListener('click', function() {
+    window.scrollTo({ top: skillsPosition.y, behavior: 'smooth' });
+});
+
+navbarLinksProjects.addEventListener('click', function() {
+    window.scrollTo({ top: projectsPosition.y, behavior: 'smooth' });
+});
+
+navbarLinksContact.addEventListener('click', function() {
+    window.scrollTo({ top: contactPosition.y, behavior: 'smooth' });
+});
+
+/* SKILLS */
+
+// DEFINE SKILL ITEMS
+const skillItems = document.querySelectorAll('.skills-item');
+
+skillItems.forEach((skill) => {
+    skill.addEventListener('click', () => {
+        skill.classList.toggle('skills-item-open');
+        skill.querySelector('p').classList.toggle('skills-paragraph-open');
+    })
+})
